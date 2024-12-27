ebada69faffb40c59192efe940f0a8c9dca33608
Caner Uçar <canerucar0@gmail.com>
2024-12-27 18:05:31 +0300

    feature : initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex h-screen justify-center items-center font-[family-name:var(--font-geist-sans)]">
+      hello world
+    </div>
+  );
+}
